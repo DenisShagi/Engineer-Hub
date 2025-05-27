d8c7e61b41d6e8540381ea19682f82a771353968
Denis Shag <denis.shagim@icloud.com>
2025-05-27 11:07:46 +0300

    feat: setting react-router-dom

--- src/features/board/board.page.tsx
+++ src/features/board/board.page.tsx
@@ -1,5 +1,15 @@
+import type { PathParams, ROUTES } from '@/shared/model/routes'
+import { useParams } from 'react-router-dom'
+
 function BoardPage() {
-  return <div>Board page</div>;
+
+  const params = useParams<PathParams[typeof ROUTES.BOARD]>()
+  return (
+    <div>
+
+      <p>Board page {params.boardId}</p>
+    </div>
+  );
 }
 
 export const Component = BoardPage;
