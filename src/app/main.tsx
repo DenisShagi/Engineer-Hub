8c57aca4d5ccfc162194ef6171074b9f077ca919
Administrator <admin@example.com>
2025-05-28 07:51:59 +0300

    feat: Configuration of configs, schemes, contracts. Query typing and optimization. Connection of the service broker

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,12 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from 'react-router-dom';
-import {router} from './router'
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
+async function enableMocking() {
+  if (import.meta.env.PROD) {
+    return;
+  }
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-);
+  const { worker } = await import("@/shared/api/mocks/browser");
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+});
